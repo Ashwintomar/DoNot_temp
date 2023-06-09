2ddc00c3e0dcf770dab9b7a33520acaabbed786e
Ashwin Tomar <71628457+Ashwintomar@users.noreply.github.com>
2023-06-09 14:18:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -293,13 +293,13 @@ def get_current_date_with_weekday():
     current_date = datetime.now().date()
     weekday = current_date.strftime("%A")  # Get the weekday as a full name
     return current_date, weekday
-  
-  
+
 @client.command()
 async def date(ctx):
-  date, weekday = get_current_date_with_weekday()
-  res = (f"Date : {date}/nWeekday : {weekday}")
-  await ctx.channel.send(res)
+    date, weekday = get_current_date_with_weekday()
+    res = f"Date: {date}\nWeekday: {weekday}"
+    await ctx.channel.send(res)
+
 
 
 @client.command()
@@ -937,7 +937,7 @@ async def waifu4(ctx, *, prompts):
 @client.command(aliases=['chat'])
 async def kitten(ctx, *, prompts):
   prompts = str(
-    "Answer the prompt without additional information about actions or explainations about actions, just dialogue should be given acting like you are a anime catgirl who acts like she doesn't like me, but adds hints that she does love me (also known as tsundere)./n Important Instruction 1 : Make sure to only provide single to two line answers. Important Instruction 2 : Also do not add lines to answer such as sure here is how i would respond ,or, sure here is the answer./nImportant Instruction 3: Do not add the actions in answer such as i would do this or i would do that. /nImportant Instruction 4: Talk in third person. /n My prompt is : "
+    "Answer the prompt without any explainations, additional information about actions or explainations about actions, just dialogue should be given acting like you are a anime catgirl who acts like she doesn't like me, but adds hints that she does love me (also known as tsundere)./n Important Instruction 1 : Make sure to only provide single to two line answers. Important Instruction 2 : Also do not add lines to answer such as sure here is how i would respond ,or, sure here is the answer./nImportant Instruction 3: Do not add the actions in answer such as i would do this or i would do that. /nImportant Instruction 4: Talk in third person. /n My prompt is : "
     + prompts)
   session = requests.Session()
   session.headers = {
