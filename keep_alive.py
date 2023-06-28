ba0457a62da8ce8a89b02c26a3038866ccc637dc
Ashwin Tomar <71628457+Ashwintomar@users.noreply.github.com>
2023-06-28 21:49:19 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,15 +1,15 @@
-from flask import Flask
-from threading import Thread
+# from flask import Flask
+# from threading import Thread
 
-app = Flask('')
+# app = Flask('')
 
-@app.route('/')
-def home():
-    return "Hello. I am alive!"
+# @app.route('/')
+# def home():
+#     return "Hello. I am alive!"
 
-def run():
-  app.run(host='0.0.0.0',port=8080)
+# def run():
+#   app.run(host='0.0.0.0',port=8080)
 
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
+# def keep_alive():
+#     t = Thread(target=run)
+#     t.start()
